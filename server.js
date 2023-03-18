c40f42bb2ef8167f62a133cf105b99bf7e63cfc2
Anders Ravn <71639005+andersravn32@users.noreply.github.com>
2023-03-18 21:53:24 +0100

    Add: Added initial server config and scaffolding

--- server.js
+++ server.js
@@ -1,20 +1,37 @@
 const express = require("express");
 const http = require("http");
 const app = express();
-
 const dotenv = require("dotenv");
-dotenv.config();
+const morgan = require("morgan");
+const helmet = require("helmet");
 
 const server = http.createServer(app);
 
-app.get("*", (req, res) => {
-  res.redirect("https://tradify.dk");
-});
-
+// Async init function
 const init = async () => {
-  server.listen(process.env.PORT);
-  console.log("Server is listening at port " + process.env.PORT);
-  console.log("CI Demo");
+  // Configure environment variables, loaded from either CI/CD or .env file
+  dotenv.config();
+
+  // Create database connection pool
+  database.connect();
+
+  // Enable morgan as primary logger
+  app.use(morgan());
+
+  // Enable helmet
+  app.use(helmet());
+
+  // Configure cors
+  app.use(cors({ origin: "*" }));
+
+  // Configure express body-parser
+  app.use(express.urlencoded({ extended: false }));
+  app.use(express.json());
+
+  // Listen to default port 3000, or port provided from environment variables
+  server.listen(process.env.PORT || 3000, () => {
+    console.log("Server is ready for requests");
+  });
 };
 
 init();
---
+++ utilities/database.js
@@ -0,0 +1,27 @@
+const { MongoClient } = require("mongodb");
+var connection = null;
+
+module.exports = {
+  connect: async () => {
+    // Create database connection
+    connection = await MongoClient.connect(process.env.MONGODB_URL);
+
+    // If connection failed, throw error
+    if (!connection) {
+      throw Error("Failed to connect to database");
+    }
+
+    // Return connection object
+    return connection.db(process.env.MONGODB_NAME);
+  },
+
+  get: () => {
+    // If no connection is present, throw error
+    if (!connection) {
+      throw Error("Failed to connect to database");
+    }
+
+    // Return connection object
+    return connection.db(process.env.MONGODB_NAME);
+  },
+};
