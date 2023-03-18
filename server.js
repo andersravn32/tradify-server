148c888222809f275571c5878c1c954feb03fb5e
Anders Ravn <anders@adriansen.dk>
2023-03-18 12:01:52 +0100

    Init: Initialized backend

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const http = require("http");
+const app = express();
+
+const dotenv = require("dotenv");
+dotenv.config();
+
+const server = http.createServer(app);
+
+app.get("*", (req, res)=> res.send("Hello world"))
+
+const init = async () => {
+
+    server.listen(process.env.PORT)
+    console.log("Server is listening at port "+ process.env.PORT)
+}
+
+init();
