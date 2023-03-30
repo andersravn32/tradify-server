6118accea48b3b50ed6682b5a7082f91fe8cccf8
Anders Ravn <anders@adriansen.dk>
2023-03-30 16:55:45 +0200

    Add: Added error handling to hcaptcha

--- middleware/check/captcha.js
+++ middleware/check/captcha.js
@@ -1,17 +1,28 @@
 const { verify } = require("hcaptcha");
+const compose = require("../../middleware/check");
 
 module.exports = async (req, res, next) => {
   const secret = process.env.CAPTCHA_SECRET;
 
   // Ensure captcha exists on request body
   if (!req.body.captcha) {
-    return;
+    // Return error
+    return res.json(
+      compose.response(null, null, [
+        { msg: "Missing captcha", location: "check" },
+      ])
+    );
   }
 
   // Verify captcha token
   const { success } = await verify(secret, req.body.captcha);
-  if (!success){
-    return;
+  if (!success) {
+    // Return error
+    return res.json(
+      compose.response(null, null, [
+        { msg: "Captcha validation failed", location: "check" },
+      ])
+    );
   }
 
   return next();
