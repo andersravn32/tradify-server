f66b554513684c04d8c67dce0dae1003f275d5ee
Anders Ravn <anders@adriansen.dk>
2023-03-30 14:51:11 +0200

    Update: Updated ecosystem file for pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     apps : [{
       name: "tradify-server",
-      script: "./dist/server.js",
+      script: "./dist/index.js",
       env: {
         NODE_ENV: "development",
       },
