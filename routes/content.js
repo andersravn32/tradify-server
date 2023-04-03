8bbac6cb1065331a7b8b808ec93429dd7119b925
Anders Ravn <anders@adriansen.dk>
2023-04-03 15:50:59 +0200

    Add: Added uploads static route

--- routes/content.js
+++ routes/content.js
@@ -1,9 +1,16 @@
 const express = require("express");
 const path = require("path");
+const check = require("../middleware/check");
 const router = express.Router();
 
 router.use(
   "/public",
+  express.static(path.join(__dirname, "../content/public"))
+);
+
+router.use(
+  "/uploads",
+  check.auth,
   express.static(path.join(__dirname, "../content/public"))
 );
 
