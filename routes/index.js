3476971fb8b5bcaa4a45300a3d4db2f4f4584234
Anders Ravn <anders@adriansen.dk>
2023-04-03 17:47:29 +0200

    Add: Added trade routes and user fetching route

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,18 @@
 const express = require("express");
 const router = express.Router();
 
+// Auth specific routes
 router.use("/auth", require("./auth"));
 
+// Routes for serving static content
 router.use("/content", require("./content"));
 
+// Singular trade specific routes
+router.use("/trade", require("./trade"));
+
+// General trades specific routes
+router.use("/trades", require("./trades"));
+
+// General user route
+router.use("/user", require("./user"));
 module.exports = router;
