27d271cc1667155724c81cd93f2950975e39b064
Anders Ravn <anders@adriansen.dk>
2023-04-03 12:56:15 +0200

    Add: Added accomodation for role objects in check role middleware

--- middleware/check/role.js
+++ middleware/check/role.js
@@ -1,7 +1,7 @@
 const { validationResult } = require("express-validator");
 const compose = require("../../utilities/compose");
 
-module.exports = (permissionLevel) => {
+module.exports = (role) => {
   return (req, res, next) => {
     const validatorErrors = validationResult(req);
     if (!validatorErrors.isEmpty()) {
@@ -9,7 +9,7 @@ module.exports = (permissionLevel) => {
     }
 
     // Ensure that user role is higher than or equal to that set as param
-    if (req.user.role.permissionLevel >= permissionLevel) {
+    if (req.user.role.permissionLevel >= role.permissionLevel) {
       return next();
     }
 
