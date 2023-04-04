774b32b1545a428cf86a56a3ff793b29a3f7ceda
Anders Ravn <anders@adriansen.dk>
2023-04-04 11:16:02 +0200

    Add: Added accept and reject options for trades

--- actions/index.js
+++ actions/index.js
@@ -1,5 +1,9 @@
 module.exports = {
     callback: {
         confirmEmail: require("./callback/confirmEmail")
+    },
+    trade: {
+        accept: require("./trade/accept"),
+        reject: require("./trade/reject")
     }
 }
