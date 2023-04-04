310fbc3bb27beece9bd45a24f8fcb9c7156b8bd1
Anders Ravn <anders@adriansen.dk>
2023-04-04 10:35:57 +0200

    Add: Added trade edit and deletion routes

--- routes/trade.js
+++ routes/trade.js
@@ -1,13 +1,36 @@
 const express = require("express");
 const router = express.Router();
 const check = require("../middleware/check");
-const { param } = require("express-validator");
+const { param, body } = require("express-validator");
+const roles = require("../content/public/roles.json");
 
+// URL: /trade/:id
 router.get(
   "/:id",
   check.auth,
   param("id").isMongoId(),
   require("../controllers/trade/trade.get")
+);
+
+// URL: /trade/:id
+router.put(
+  "/:id",
+  check.auth,
+  check.role(roles.member),
+  body("title").isLength({ min: 5, max: 50 }),
+  body("description").isLength({ min: 25, max: 500 }),
+  body("to").isObject(),
+  body("middleman").isObject(),
+  param("id").isMongoId(),
+  require("../controllers/trade/trade.put")
+);
+
+// URL: /trade/:id
+router.delete(
+  "/:id",
+  check.auth,
+  param("id").isMongoId(),
+  require("../controllers/trade/trade.delete")
 );
 
 module.exports = router;
