3ca300cb5c0222848dacbd15a83285e32d3d6efd
Anders Ravn <anders@adriansen.dk>
2023-04-04 13:32:03 +0200

    Add: Added rate action guards

--- actions/trade/rate.js
+++ actions/trade/rate.js
@@ -2,11 +2,29 @@ const { ObjectId } = require("mongodb");
 const database = require("../../utilities/database");
 
 module.exports = async (trade, user, rating) => {
+  // If trade has already been completed
+  if (trade.completed) {
+    return false;
+  }
+
+  // Ensure that all participants in the trade has confirmed
+  if (
+    (trade.from.uuid && !trade.from.confirmed) ||
+    (trade.to.uuid && !trade.to.confirmed) ||
+    (trade.middleman.uuid && !trade.middleman.confirmed)
+  ) {
+    return false;
+  }
+
   // Create correct database query, based on user role in trade
   let query = null;
 
   // User has from role in trade
   if (user.uuid == trade.from.uuid && trade.from.confirmed) {
+    // If trade parameter: from, already has rating, return false
+    if (trade.from.rating) {
+      return false;
+    }
     query = {
       "from.rating": rating,
     };
@@ -14,6 +32,10 @@ module.exports = async (trade, user, rating) => {
 
   // User has to role in trade
   if (user.uuid == trade.to.uuid && trade.to.confirmed) {
+    // If trade parameter: to, already has rating, return false
+    if (trade.to.rating) {
+      return false;
+    }
     query = {
       "to.rating": rating,
     };
@@ -21,6 +43,10 @@ module.exports = async (trade, user, rating) => {
 
   // User has middleman role in trade
   if (user.uuid == trade.middleman.uuid && trade.middleman.confirmed) {
+    // If trade parameter: middleman, already has rating, return false
+    if (trade.middleman.rating) {
+      return false;
+    }
     query = {
       "middleman.rating": rating,
     };
@@ -31,12 +57,11 @@ module.exports = async (trade, user, rating) => {
     return false;
   }
 
-  console.log(query);
-
   try {
     // Get database connection
     const db = database.get();
 
+    // Update rating in database
     const updateQuery = await db.collection("trades").updateOne(
       { _id: new ObjectId(trade._id) },
       {
@@ -47,7 +72,7 @@ module.exports = async (trade, user, rating) => {
     if (!updateQuery.modifiedCount) {
       return false;
     }
-    
+
     // Locate updated trade in database
     const updatedTrade = await db
       .collection("trades")
@@ -83,7 +108,7 @@ module.exports = async (trade, user, rating) => {
         },
       }
     );
-      
+
     // If query could not update anything, return error
     if (!completeTrade.modifiedCount) {
       return false;
@@ -95,5 +120,6 @@ module.exports = async (trade, user, rating) => {
       .findOne({ _id: new ObjectId(trade._id) });
   } catch (error) {
     console.log(error);
+    return false;
   }
 };
